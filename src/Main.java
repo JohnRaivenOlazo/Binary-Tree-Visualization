5a3978b5a25d9e88bc3a7328a7e8980eac3f379e
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-05-05 17:29:41 +0800

    Init Choices in Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-
-		//Main to
-	}
-
+    public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+    	
+    	System.out.print("Enter your name: ");
+    	String name = s.nextLine();
+    	System.out.println("Hello " + name + "\n\n");
+    	System.out.println("Tell me what you want to do. Here are your options:\n\n");
+    	
+    	System.out.println("[1] Determine if a year is a LEAP YEAR or NOT");
+    	System.out.println("[2] Height Comparison of 3 person");
+    	System.out.println("[3] Display a series");
+    	System.out.println("[4] Display a diamond of asterisk");
+    	
+    	int options = s.nextInt();
+        
+        switch (options) {
+            case 1:
+                //leapYear();
+                break;
+            case 2:
+                //heightComparison();
+                break;
+            case 3:
+                //series();
+                break;
+            case 4:
+                //diamond();
+                break;
+            default:
+                System.out.println("Choice was not on the list !");
+        }
+    }
 }
